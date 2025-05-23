879a273e1a174e0e2b710b4a343ed6b50d019482
maramvenkatareddy <113689196+maramvenkatareddy@users.noreply.github.com>
2025-05-23 16:05:05 +0530

    Create User.java

---
+++ src/main/java/com/example/User.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User {
+    @JsonProperty("id")
+    private Long id;
+    
+    @JsonProperty("username")
+    private String username;
+    
+    @JsonProperty("email")
+    private String email;
+
+    public User() {}
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public User(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    @Override
+    public String toString() {
+        return "User{id=" + id + ", username='" + username + "', email='" + email + "'}";
+    }
+}
